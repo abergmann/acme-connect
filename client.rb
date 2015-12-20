7a5efa34c1b81b0d7e93691e0d48f016d87e541e
Alexander Bergmann <abergmann@suse.com>
2015-12-20 20:30:55 +0000

    Added some comments.

--- client.rb
+++ client.rb
@@ -9,18 +9,22 @@ aggrement_url = "https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf
 # Here you get the real stuff
 #endpoint_url = "https://acme-v01.api.letsencrypt.org/"
 
+# Initialize ACME Connection
 acme = ACME_Connect.new(endpoint_url, aggrement_url)
 acme.account('account.key','hostmaster@example.com')
 acme.register
 
-out = acme.authorize("www.example.com")
-acme.push_response("/srv/www/www.example.com/.well-known/acme-challenge", out["token"])
-acme.verify(out["token"], out["uri"])
+# Request Domain authorization
+authz = acme.authorize("www.example.com")
+acme.push_response("/srv/www/www.example.com/.well-known/acme-challenge", authz["token"])
+acme.verify(authz["token"], authz["uri"])
 
+# Wait for verification finished
 while !acme.verify_check(out["uri"])
   sleep 1
 end
 
+# Download Certificate
 cert = acme.get_cert('domain.key', 'www.example.com')
 File.write('domain.pem', cert)
 
